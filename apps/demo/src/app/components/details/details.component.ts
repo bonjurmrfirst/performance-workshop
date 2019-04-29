2d4101777d245d3446349dfd5b4756f155b58d54
Illia Potsepaiev <illia_potsepaiev@epam.com>
2019-04-30 00:52:53 +0300

    clean up details.component

--- apps/demo/src/app/components/details/details.component.ts
+++ apps/demo/src/app/components/details/details.component.ts
@@ -1,18 +1,9 @@
 import {
-  AfterViewInit,
   Component,
-  ElementRef, Input, OnDestroy,
-  OnInit,
-  QueryList,
-  ViewChildren
+  Input,
+  OnInit
 } from '@angular/core';
-import { select, Store } from '@ngrx/store';
-import * as fromGrid from '../../+state/grid.reducer';
-import { Subscription } from 'rxjs';
 import { Target } from '@performance-workshop/shared';
-import Dygraph from 'dygraphs';
-import { getLiveUpdates } from '../../+state/grid.reducer';
-import { map } from 'rxjs/operators';
 import * as clone from 'clone';
 
 @Component({
@@ -20,7 +11,7 @@ import * as clone from 'clone';
   templateUrl: './details.component.html',
   styleUrls: ['./details.component.scss']
 })
-export class DetailsComponent implements OnInit, OnDestroy {
+export class DetailsComponent implements OnInit {
 
   private _id: string;
 
@@ -35,10 +26,10 @@ export class DetailsComponent implements OnInit, OnDestroy {
 
   @Input() public grid: Target[];
 
-  processedGrid: Target[];
-  lastUpdatedAt: Date;
-x;s;
-  constructor() {}
+  public warningMsg: string;
+  public processedGrid: Target[];
+
+  private lastUpdatedAt: Date;
 
   public ngOnInit(): void {
     this.lastUpdatedAt = new Date();
@@ -47,18 +38,14 @@ x;s;
 
     this.processedGrid[0].data.map((record, i, data) => i === 0 ? record : record + data[i - 1]);
 
-    this.x = () => {
+    window.addEventListener('click', () => {
+      const name = this.processedGrid.find(target => target.id === this.id).name;
+      const expiration = Math.round((new Date().getTime() - this.lastUpdatedAt.getTime()) / 1000);
+
       setInterval(() => {
-        this.s = `Details of ${this.processedGrid.find(target => target.id === this.id).name} might be outdated. Last updated ${Math.round((new Date().getTime() - this.lastUpdatedAt.getTime()) / 1000)} seconds ago`;
-        console.log('1');
+        this.warningMsg = `Details of ${name} might be outdated. Last updated ${expiration} seconds ago`;
       }, 1000);
-    };
-
-    window.addEventListener('click', this.x);
-  }
-
-  public ngOnDestroy(): void {
-    document.removeEventListener('copy', this.x);
+    });
   }
 
 }
