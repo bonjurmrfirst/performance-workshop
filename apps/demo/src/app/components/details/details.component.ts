8cd4b9670d92f3614deeac726d2c8150ef3f4120
Illia Potsepaiev <illia_potsepaiev@epam.com>
2019-04-29 22:01:25 +0300

    init memory leak

--- apps/demo/src/app/components/details/details.component.ts
+++ apps/demo/src/app/components/details/details.component.ts
@@ -13,6 +13,7 @@ import { Target } from '@performance-workshop/shared';
 import Dygraph from 'dygraphs';
 import { getLiveUpdates } from '../../+state/grid.reducer';
 import { map } from 'rxjs/operators';
+import * as clone from 'clone';
 
 @Component({
   selector: 'performance-workshop-details',
@@ -24,42 +25,27 @@ export class DetailsComponent implements OnInit {
   @Input() public id: string;
   @Input() public grid: Target[];
 
-  constructor(
-    private store: Store<fromGrid.GridState>
-  ) {}
+  processedGrid: Target[];
+  lastUpdatedAt: Date;
+x;s;
+  constructor() {}
 
   public ngOnInit(): void {
-    this.store.pipe(
-      select(fromGrid.getGrid),
-      map(grid => {
-        console.log('calc');
-          const selectedTarget = grid.find(target => target.id === this.id);
+    this.lastUpdatedAt = new Date();
 
-          return selectedTarget.data.reduce(
-            (accTotal, targetDataNumber) => accTotal += grid
-              .filter(target => target.id !== this.id)
-              .reduce((acc, target) => acc += target.data.filter(number => number === targetDataNumber).length, 0),
-            0)
-        }
-      ))
-      .subscribe(r => {
-        setInterval(() => {
-          console.log(r);
-          console.log(this.grid);
-        }, 1000);
-      });
+    this.processedGrid = clone(this.grid).sort(a => a.id === this.id ? -1 : 1);
+
+    this.processedGrid[0].data.map((record, i, data) => i === 0 ? record : record + data[i - 1]);
+
+    this.x = () => {
+      setInterval(() => this.s = `Details of ${this.processedGrid.find(target => target.id === this.id).name} might be outdated. Last updated at ${this.lastUpdatedAt.toLocaleTimeString()}`, 1000);
+    };
+
+    window.addEventListener('scroll', this.x);
   }
 
   public ngOnDestroy(): void {
-
+    document.removeEventListener('copy', this.x);
   }
 
 }
-
-/*
-return selectedTarget.data.reduce(
-            (accTotal, targetDataNumber) => accTotal += grid
-              .filter(target => target.id !== this.id)
-              .reduce((acc, target) => acc += target.data.filter(number => number === targetDataNumber).length, 0),
-            0)
- */
